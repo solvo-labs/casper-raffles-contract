ae53f52924fc993b50b5bc8badb08d9ad70aa3ea
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-03-15 02:33:32 +0300

    cancel function date control

--- raffle/src/raffle.rs
+++ raffle/src/raffle.rs
@@ -1,36 +1,27 @@
 use core::ops::Add;
 
-use alloc::{ string::{ String, ToString }, vec };
+use alloc::{
+    string::{String, ToString},
+    vec,
+};
 
 use crate::{
-    error::Error,
-    utils::{ get_current_address, get_key, self },
-    events::{ emit, RaffleEvent },
     enums::Address,
+    error::Error,
+    events::{emit, RaffleEvent},
+    utils::{self, get_current_address, get_key},
 };
 
 use casper_types::{
-    account::AccountHash,
-    EntryPoint,
-    Key,
-    ContractHash,
-    EntryPointAccess,
-    CLType,
-    EntryPointType,
-    EntryPoints,
-    contracts::NamedKeys,
+    account::AccountHash, contracts::NamedKeys, runtime_args, CLType, CLValue, ContractHash,
+    EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, Key, Parameter, RuntimeArgs, URef,
     U512,
-    RuntimeArgs,
-    runtime_args,
-    URef,
-    CLValue,
-    Parameter,
 };
 
-use casper_contract::contract_api::{ runtime, storage, system };
+use casper_contract::contract_api::{runtime, storage, system};
 use casper_contract::unwrap_or_revert::UnwrapOrRevert;
 
-use tiny_keccak::{ Sha3, Hasher };
+use tiny_keccak::{Hasher, Sha3};
 
 // variables
 const NAME: &str = "name";
@@ -59,8 +50,14 @@ const ENTRY_POINT_CANCEL: &str = "cancel";
 
 #[no_mangle]
 pub extern "C" fn cancel() {
-    //check end date for raffle
     check_admin_account();
+
+    let now: u64 = runtime::get_blocktime().into();
+    let end_date: u64 = utils::read_from(END_DATE);
+
+    if end_date.lt(&now) {
+        runtime::revert(Error::TimeError);
+    }
 
     let partipiciant_count: u64 = utils::read_from(PARTIPICANT_COUNT);
     let collection: Key = utils::read_from(COLLECTION);
@@ -70,14 +67,17 @@ pub extern "C" fn cancel() {
         runtime::revert(Error::CancelError);
     }
 
-    let now: u64 = runtime::get_blocktime().into();
-
     let collection_hash: ContractHash = collection.into_hash().map(ContractHash::new).unwrap();
 
     let caller: AccountHash = runtime::get_caller();
     let contract_address = get_current_address();
 
-    transfer(collection_hash, contract_address.into(), caller.into(), token_id);
+    transfer(
+        collection_hash,
+        contract_address.into(),
+        caller.into(),
+        token_id,
+    );
     runtime::put_key(CLAIMED, storage::new_uref(true).into());
     runtime::put_key(END_DATE, storage::new_uref(now).into());
 }
@@ -120,7 +120,11 @@ pub extern "C" fn draw() {
 
     system::transfer_from_purse_to_account(contract_purse, owner, balance, None).unwrap();
 
-    emit(&(RaffleEvent::Draw { winner: random_winner }))
+    emit(
+        &(RaffleEvent::Draw {
+            winner: random_winner,
+        }),
+    )
 }
 
 #[no_mangle]
@@ -141,11 +145,21 @@ pub extern "C" fn buy_ticket() {
 
     let partipiciant_count: u64 = utils::read_from(PARTIPICANT_COUNT);
 
-    let partipiciant_dict = *runtime::get_key(PARTIPICANT_DICT).unwrap().as_uref().unwrap();
+    let partipiciant_dict = *runtime::get_key(PARTIPICANT_DICT)
+        .unwrap()
+        .as_uref()
+        .unwrap();
 
-    storage::dictionary_put(partipiciant_dict, &partipiciant_count.to_string(), partipiciant);
+    storage::dictionary_put(
+        partipiciant_dict,
+        &partipiciant_count.to_string(),
+        partipiciant,
+    );
 
-    runtime::put_key(PARTIPICANT_COUNT, storage::new_uref(partipiciant_count.add(1u64)).into());
+    runtime::put_key(
+        PARTIPICANT_COUNT,
+        storage::new_uref(partipiciant_count.add(1u64)).into(),
+    );
 
     emit(&(RaffleEvent::BuyTicket { partipiciant }))
 }
@@ -181,11 +195,13 @@ pub extern "C" fn claim() {
 
     let collection_hash: ContractHash = collection.into_hash().map(ContractHash::new).unwrap();
 
-    let partipiciant_dict = *runtime::get_key(PARTIPICANT_DICT).unwrap().as_uref().unwrap();
+    let partipiciant_dict = *runtime::get_key(PARTIPICANT_DICT)
+        .unwrap()
+        .as_uref()
+        .unwrap();
     let winner: u64 = utils::read_from(WINNER);
 
-    let winner_partipiciant: Key = storage
-        ::dictionary_get(partipiciant_dict, &winner.to_string())
+    let winner_partipiciant: Key = storage::dictionary_get(partipiciant_dict, &winner.to_string())
         .unwrap()
         .unwrap_or_revert_with(Error::WinnerError);
 
@@ -193,11 +209,22 @@ pub extern "C" fn claim() {
         runtime::revert(Error::WinnerError);
     }
 
-    transfer(collection_hash, contract_address.into(), winner_partipiciant, token_id);
+    transfer(
+        collection_hash,
+        contract_address.into(),
+        winner_partipiciant,
+        token_id,
+    );
 
     runtime::put_key(CLAIMED, storage::new_uref(true).into());
 
-    emit(&(RaffleEvent::Claim { winner_partipiciant, collection, token_id }));
+    emit(
+        &(RaffleEvent::Claim {
+            winner_partipiciant,
+            collection,
+            token_id,
+        }),
+    );
 }
 
 // admin function
@@ -212,19 +239,22 @@ pub extern "C" fn deposit() {
 
     let collection_hash: ContractHash = collection.into_hash().map(ContractHash::new).unwrap();
 
-    get_approved(collection_hash, caller.into(), token_id).unwrap_or_revert_with(
-        Error::NotApproved
-    );
+    get_approved(collection_hash, caller.into(), token_id)
+        .unwrap_or_revert_with(Error::NotApproved);
 
     // check owner is caller
-    transfer(collection_hash, caller.into(), contract_address.into(), token_id);
+    transfer(
+        collection_hash,
+        caller.into(),
+        contract_address.into(),
+        token_id,
+    );
 
     storage::new_dictionary(PARTIPICANT_DICT).unwrap_or_default();
 
     runtime::put_key(PARTIPICANT_COUNT, storage::new_uref(0u64).into());
 }
 
-//
 #[no_mangle]
 pub extern "C" fn call() {
     let name: String = runtime::get_named_arg(NAME);
@@ -247,14 +277,17 @@ pub extern "C" fn call() {
     named_keys.insert(OWNER.to_string(), storage::new_uref(owner).into());
     named_keys.insert(COLLECTION.to_string(), storage::new_uref(collection).into());
     named_keys.insert(NFT_INDEX.to_string(), storage::new_uref(nft_index).into());
-    named_keys.insert(STORAGE_KEY.to_string(), storage::new_uref(storage_key).into());
+    named_keys.insert(
+        STORAGE_KEY.to_string(),
+        storage::new_uref(storage_key).into(),
+    );
 
     let draw_entry_point = EntryPoint::new(
         ENTRY_POINT_DRAW,
         vec![],
         CLType::URef,
         EntryPointAccess::Public,
-        EntryPointType::Contract
+        EntryPointType::Contract,
     );
 
     let claim_entry_point = EntryPoint::new(
@@ -262,7 +295,7 @@ pub extern "C" fn call() {
         vec![],
         CLType::URef,
         EntryPointAccess::Public,
-        EntryPointType::Contract
+        EntryPointType::Contract,
     );
 
     let deposit_entry_point = EntryPoint::new(
@@ -270,7 +303,7 @@ pub extern "C" fn call() {
         vec![],
         CLType::URef,
         EntryPointAccess::Public,
-        EntryPointType::Contract
+        EntryPointType::Contract,
     );
 
     let get_price_entry_point = EntryPoint::new(
@@ -278,7 +311,7 @@ pub extern "C" fn call() {
         vec![],
         CLType::U512,
         EntryPointAccess::Public,
-        EntryPointType::Contract
+        EntryPointType::Contract,
     );
 
     let get_purse_entry_point = EntryPoint::new(
@@ -286,7 +319,7 @@ pub extern "C" fn call() {
         vec![],
         CLType::URef,
         EntryPointAccess::Public,
-        EntryPointType::Contract
+        EntryPointType::Contract,
     );
 
     let buy_ticket_entry_point = EntryPoint::new(
@@ -294,7 +327,7 @@ pub extern "C" fn call() {
         vec![Parameter::new(PARTIPICANT, CLType::Key)],
         CLType::URef,
         EntryPointAccess::Public,
-        EntryPointType::Contract
+        EntryPointType::Contract,
     );
 
     let cancel_entry_point = EntryPoint::new(
@@ -302,7 +335,7 @@ pub extern "C" fn call() {
         vec![],
         CLType::URef,
         EntryPointAccess::Public,
-        EntryPointType::Contract
+        EntryPointType::Contract,
     );
 
     let mut entry_points = EntryPoints::new();
@@ -327,7 +360,7 @@ pub extern "C" fn call() {
         entry_points,
         Some(named_keys),
         Some(hash_name.to_string()),
-        Some(uref_name.to_string())
+        Some(uref_name.to_string()),
     );
 
     runtime::put_key(&contract_hash_text.to_string(), contract_hash.into());
@@ -336,8 +369,8 @@ pub extern "C" fn call() {
         storage_key,
         "insert",
         runtime_args! {
-        "data" => contract_hash.to_string(),
-    }
+            "data" => contract_hash.to_string(),
+        },
     );
 }
 
@@ -362,9 +395,9 @@ pub fn get_approved(contract_hash: ContractHash, owner: Address, token_id: u64) 
         contract_hash,
         "get_approved",
         runtime_args! {
-        "owner" => owner,
-        "token_id" => token_id
-      }
+          "owner" => owner,
+          "token_id" => token_id
+        },
     )
 }
 
@@ -373,9 +406,9 @@ pub fn transfer(contract_hash: ContractHash, sender: Key, recipient: Key, token_
         contract_hash,
         "transfer",
         runtime_args! {
-          "token_id" => token_id,
-          "source_key" => sender,
-          "target_key" => recipient,
-      }
+            "token_id" => token_id,
+            "source_key" => sender,
+            "target_key" => recipient,
+        },
     )
 }
